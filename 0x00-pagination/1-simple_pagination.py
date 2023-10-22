de14abf46d181e712a8bab36616fe5937eb2c634
Usman Adepoju <adepojuusman465@gmail.com>
2023-10-22 14:08:57 +0100

    Add solution to task-1

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -15,8 +15,7 @@ def index_range(page: int, page_size: int) -> Tuple[int, int]:
 
 
 class Server:
-    """Server class to paginate a database of popular baby names.
-    """
+    """Server class to paginate a database of popular baby names"""
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
