7223bfdd0e86475f7b9f76e90dfcf9c7ce1f5e89
Usman Adepoju <adepojuusman465@gmail.com>
2023-10-25 07:01:02 +0100

    Add solution to task-1

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,38 +1,28 @@
 #!/usr/bin/env python3
-"""FIFO caching system"""
+"""FIFOCache caching system"""
 
 from base_caching import BaseCaching
 
 
 class FIFOCache(BaseCaching):
-    """Class FIFOCache for FIFO cache system"""
+    """FIFO Cache class"""
     def __init__(self):
         """init method"""
         super().__init__()
 
     def put(self, key, item):
-        """Assign value to the given key"""
+        """Assign value to a given key"""
         if key is None or item is None:
             return
-        else:
-            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                keys = self.cache_data.keys()
-                f_key = ''
-                for i, k in enumerate(keys):
-                    if i == 0:
-                        f_key = k
-                        break
-                if key not in keys:
-                    del self.cache_data[f_key]
-                    print(f"DISCARD {f_key}")
 
-                self.cache_data[key] = item
-            else:
-                self.cache_data[key] = item
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            if key not in self.cache_data.keys():
+                f_key = next(iter(self.cache_data))
+                del self.cache_data[f_key]
+                print(f"DISCARD {f_key}")
+
+        self.cache_data[key] = item
 
     def get(self, key):
-        """Get value for a given key"""
-        try:
-            return self.cache_data[key]
-        except KeyError:
-            return None
+        """Get value of a given key"""
+        return self.cache_data.get(key)
