522a101d99554127bbefa884e84dcb82dd2bb63f
Usman Adepoju <adepojuusman465@gmail.com>
2023-10-25 21:44:11 +0100

    Add solutio

---
+++ 0x01-caching/100-lfu_cache.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+"""LFU Cache system"""
+
+from collections import defaultdict
+from datetime import datetime
+from base_caching import BaseCaching
+
+
+class LFUCache(BaseCaching):
+    """LFU Cache class"""
+    def __init__(self):
+        """init method"""
+        super().__init__()
+        self.frequency = defaultdict(int)  # Track the usage frequency of each key
+        self.access_time = {}  # Track the access time of each key
+
+    def put(self, key, item):
+        """Assign value for a given key"""
+        if key is None or item is None:
+            return
+
+        # Check if cache is at maximum capacity
+        if len(self.cache_data) >= self.MAX_ITEMS:
+            least_frequency = min(self.frequency.values())
+
+            # Find all keys with the least frequency
+            least_frequent_keys = [k for k, v in self.frequency.items() if v == least_frequency]
+
+            if len(least_frequent_keys) == 1:
+                # If there is only one key with the least frequency, remove it
+                discarded_key = least_frequent_keys[0]
+            else:
+                # If there are multiple keys with the least frequency, use LRU to break the tie
+                discarded_key = min(least_frequent_keys, key=lambda k: self.access_time[k])
+
+            self.frequency.pop(discarded_key)
+            self.access_time.pop(discarded_key)
+            self.cache_data.pop(discarded_key)
+            print("DISCARD:", discarded_key)
+
+        self.cache_data[key] = item
+        self.frequency[key] += 1
+        self.access_time[key] = datetime.now()
+
+    def get(self, key):
+        """Get value for a given key"""
+        if key is None or key not in self.cache_data:
+            return None
+
+        self.frequency[key] += 1
+        self.access_time[key] = datetime.now()
+        return self.cache_data[key]
