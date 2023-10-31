d172a3133efd16210068c7f6ed508aafb49c1388
Usman Adepoju <adepojuusman465@gmail.com>
2023-10-31 15:32:49 +0100

    Add basic flask setup for task-0

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -7,10 +7,10 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def index():
+def index_page() -> str:
     """index route"""
     return render_template('0-index.html')
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0', port=5000)
