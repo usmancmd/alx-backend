af7601c8a121eb625dc524f7dfa75e23a3f3bfaf
Usman Adepoju <adepojuusman465@gmail.com>
2023-11-27 15:31:33 +0100

    Add 1-redis_op.js

---
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -0,0 +1,26 @@
+import { createClient } from 'redis';
+
+const client = createClient()
+
+client.on('error', err => console.log('Redis client not connected to the server: ', err))
+
+client.on('connect', () => {
+      console.log('Redis client connected to the server')
+  });
+
+
+const setNewSchool = (schoolName, value) => {
+    client.set(schoolName, value, (err, reply) => {
+        console.log(`reply: ${reply}`) // ok
+    })
+}
+
+const displaySchoolValue = (schoolName) => {
+    client.get(schoolName, (err, reply) => {
+        console.log(reply) // log the value of key schoolName
+    })
+}
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
